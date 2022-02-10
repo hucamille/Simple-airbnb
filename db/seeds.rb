3fd088005c5175aa29f8af1016c9fc6d45e47c5a
Camille Hug <hug.camille@hotmail.fr>
2022-02-10 09:22:26 +0100

    Add 4 Flat

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,6 @@ Flat.destroy_all
   pic = "https://source.unsplash.com/300x300/?flats"
   flat = Flat.create!(name: Faker::Address.city,
                       address: Faker::Address.street_address,
-                      picture_url: pic,
                       description: 'brand new flat for renting',
                       price_per_night: rand(150..500),
                       number_of_guests: rand(1..5))
