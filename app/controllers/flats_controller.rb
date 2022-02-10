8b44b86b49e91c7424af867f7b5bd075d4066ac0
Camille Hug <hug.camille@hotmail.fr>
2022-02-10 19:57:21 +0100

    edit done

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,11 +1,10 @@
 class FlatsController < ApplicationController
-
+  before_action :find_flat, only: [:show, :edit, :update, :destroy]
   def index
     @flats = Flat.all
   end
 
   def show
-    @flat = Flat.find(params[:id])
   end
 
   def new
@@ -19,17 +18,27 @@ class FlatsController < ApplicationController
     redirect_to flat_path(@flat)
   end
 
-  def update
-    @flat = Flat.find(params[:id])
+  def edit
+  end
 
+  def update
     @flat.update(flat_params)
     redirect_to flat_path
   end
 
+  def destroy
+    @flat.destroy
+
+    redirect_to flats_path
+  end
+
   private
 
-  def flat_params
-    params.require(:restaurant).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
 
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 end
